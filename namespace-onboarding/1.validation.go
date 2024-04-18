06f7d7156cb287c6b5a8e17a258144283b2d6984
David Gardiner <davidmarkgardiner@gmail.com>
2024-04-18 12:23:20 +0100

    Add namespace-onboarding files and update useful_cmds

---
+++ namespace-onboarding/1.validation.go
@@ -0,0 +1,257 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	// "io"
+	"os"
+	"regexp"
+	"testing"
+)
+
+type Data struct {
+	Action             string `json:"action"`
+	Swci               string `json:"swci"`
+	Suffix             string `json:"suffix"`
+	Region             string `json:"region"`
+	OpEnvironment      string `json:"opEnvironment"`
+	ResourceQuota      string `json:"resourceQuota"`
+	BillingReference   string `json:"billingReference"`
+	Source             string `json:"source"`
+	Swc                string `json:"swc"`
+	DataClassification string `json:"dataClassification"`
+	AppSubdomain       string `json:"appSubdomain"`
+	AllowAccessFromNS  string `json:"allowAccessFromNS"`
+	RequestedBy        string `json:"requestedBy"`
+}
+
+// go throuhg all files it data folder and validate the json
+func TestJsonValidationAllFiles(t *testing.T) {
+	files, err := os.ReadDir("../kustomize/data2/")
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, file := range files {
+		jsonFile, err := os.Open("../kustomize/data2/" + file.Name())
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer jsonFile.Close()
+
+		// byteValue, err := io.ReadAll(jsonFile)
+		// if err != nil {
+		// 	t.Fatal(err)
+		// }
+
+		// var data Data
+		// err = json.Unmarshal(byteValue, &data)
+		// if err != nil {
+		// 	t.Fatal(err)
+		// }
+		var data Data
+		err = json.NewDecoder(jsonFile).Decode(&data)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !isValidAction(data.Action) || !isValidSwci(data.Swci) || !isValidSuffix(data.Suffix) ||
+			!isValidRegion(data.Region) || !isValidOpEnvironment(data.OpEnvironment) ||
+			!isValidResourceQuota(data.ResourceQuota) || !isValidBillingReference(data.BillingReference) ||
+			!isValidRequestedBy(data.RequestedBy) || !isValidAllowAccessFromNS(data.AllowAccessFromNS) ||
+			!isValidDataClassification(data.DataClassification) {
+			t.Fail()
+		}
+		t.Logf("Action: %s\n", data.Action)
+		t.Logf("Swci: %s\n", data.Swci)
+		t.Logf("Suffix: %s\n", data.Suffix)
+		t.Logf("Region: %s\n", data.Region)
+		t.Logf("OpEnvironment: %s\n", data.OpEnvironment)
+		t.Logf("ResourceQuota: %s\n", data.ResourceQuota)
+		t.Logf("BillingReference: %s\n", data.BillingReference)
+		t.Logf("Source: %s\n", data.Source)
+		t.Logf("Swc: %s\n", data.Swc)
+		t.Logf("DataClassification: %s\n", data.DataClassification)
+		t.Logf("AppSubdomain: %s\n", data.AppSubdomain)
+		t.Logf("AllowAccessFromNS: %s\n", data.AllowAccessFromNS)
+		t.Logf("RequestedBy: %s\n", data.RequestedBy)
+	}
+}
+
+// func TestJsonValidation(t *testing.T) {
+// 	jsonFile, err := os.Open("data.json")
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	defer jsonFile.Close()
+
+// 	byteValue, err := io.ReadAll(jsonFile)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	var data Data
+// 	err = json.Unmarshal(byteValue, &data)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	if !isValidAction(data.Action) || !isValidSwci(data.Swci) || !isValidSuffix(data.Suffix) ||
+// 		!isValidRegion(data.Region) || !isValidOpEnvironment(data.OpEnvironment) ||
+// 		!isValidResourceQuota(data.ResourceQuota) || !isValidBillingReference(data.BillingReference) ||
+// 		!isValidRequestedBy(data.RequestedBy) || !isValidAllowAccessFromNS(data.AllowAccessFromNS) ||
+// 		!isValidDataClassification(data.DataClassification) {
+// 		t.Fail()
+// 	}
+// 	t.Logf("Action: %s\n", data.Action)
+// 	t.Logf("Swci: %s\n", data.Swci)
+// 	t.Logf("Suffix: %s\n", data.Suffix)
+// 	t.Logf("Region: %s\n", data.Region)
+// 	t.Logf("OpEnvironment: %s\n", data.OpEnvironment)
+// 	t.Logf("ResourceQuota: %s\n", data.ResourceQuota)
+// 	t.Logf("BillingReference: %s\n", data.BillingReference)
+// 	t.Logf("Source: %s\n", data.Source)
+// 	t.Logf("Swc: %s\n", data.Swc)
+// 	t.Logf("DataClassification: %s\n", data.DataClassification)
+// 	t.Logf("AppSubdomain: %s\n", data.AppSubdomain)
+// 	t.Logf("AllowAccessFromNS: %s\n", data.AllowAccessFromNS)
+// 	t.Logf("RequestedBy: %s\n", data.RequestedBy)
+// }
+
+// verify that data.Action is one of the allowed values ("Add", "Remove", or "Update")
+func isValidAction(action string) bool {
+	switch action {
+	case "Add", "Remove", "Update":
+		return true
+	default:
+		fmt.Printf("Invalid action: %s. Allowed values are Add, Remove, Update.\n", action)
+		return false
+	}
+}
+
+// verify that data.Swci starts with "at" and is followed by exactly 5 numbers,
+func isValidSwci(swci string) bool {
+	match, err := regexp.MatchString(`^at\d{5}$`, swci)
+	if err != nil {
+		fmt.Println("Error while matching regex:", err)
+		return false
+	}
+	if match {
+		return true
+	} else {
+		fmt.Printf("Invalid Swci: %s. It should start with 'at' followed by exactly 5 numbers.\n", swci)
+		return false
+	}
+}
+
+// verify that data.Suffix is a maximum of 8 characters and contains only numbers, letters, and hyphens
+func isValidSuffix(suffix string) bool {
+	match, _ := regexp.MatchString(`^[a-zA-Z0-9-]{1,8}$`, suffix)
+	if match {
+		fmt.Printf("Suffix: %s\n", suffix)
+		return true
+	} else {
+		fmt.Printf("Invalid Suffix: %s. It should be a maximum of 8 characters and contain only numbers, letters, and hyphens.\n", suffix)
+		return false
+	}
+}
+
+// verify that data.Region is one of the allowed values ("westeurope", "eastus2", or "centralus")
+func isValidRegion(region string) bool {
+	switch region {
+	case "westeurope", "eastus2", "centralus":
+		fmt.Printf("Region: %s\n", region)
+		return true
+	default:
+		fmt.Printf("Invalid region: %s. Allowed values are westeurope, eastus2, centralus.\n", region)
+		return false
+	}
+}
+func isValidOpEnvironment(opEnvironment string) bool {
+	// verify that data.OPEnvironment is one of the allowed values ("dev", "prod", "te1", or "te2"),
+	switch opEnvironment {
+	case "dev", "prod", "te1", "te2":
+		fmt.Printf("OPEnvironment: %s\n", opEnvironment)
+		return true
+	default:
+		fmt.Printf("Invalid OPEnvironment: %s. Allowed values are dev, prod, te1, te2.\n", opEnvironment)
+		return false
+	}
+}
+
+// data.ResourceQuota is one of the allowed values ("small", "medium", or "large")
+func isValidResourceQuota(resourceQuota string) bool {
+	switch resourceQuota {
+	case "small", "medium", "large":
+		fmt.Printf("ResourceQuota: %s\n", resourceQuota)
+		return true
+	default:
+		fmt.Printf("Invalid ResourceQuota: %s. Allowed values are small, medium, large.\n", resourceQuota)
+		return false
+	}
+}
+
+// verify that data.BillingReference is in the format "AB-BC-ABCDE-ABCDE" and contains only letters,
+func isValidBillingReference(billingReference string) bool {
+	match, _ := regexp.MatchString(`^[A-Za-z]{2}-[A-Za-z]{2}-[A-Za-z]{5}-[A-Za-z]{5}$`, billingReference)
+	if match {
+		fmt.Printf("BillingReference: %s\n", billingReference)
+		return true
+	} else {
+		fmt.Printf("Invalid BillingReference: %s. It should be in the format AB-BC-DEPT-ABCDE and contain only letters.\n", billingReference)
+		return false
+	}
+}
+
+// verify that data.RequestedBy is an email like "david.gardiner@ubs.com", a GPN like "43709651", or a T number like "t69332"
+func isValidRequestedBy(requestedBy string) bool {
+	emailMatch, _ := regexp.MatchString(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`, requestedBy)
+	gpnMatch, _ := regexp.MatchString(`^[0-9]{8}$`, requestedBy)
+	tNumberMatch, _ := regexp.MatchString(`^t[0-9]{5}$`, requestedBy)
+
+	if emailMatch || gpnMatch || tNumberMatch {
+		fmt.Printf("RequestedBy: %s\n", requestedBy)
+		return true
+	} else {
+		fmt.Printf("Invalid RequestedBy: %s. It should be an email like david.gardiner@ubs.com, a GPN like 43709651, or a T number like t69332.\n", requestedBy)
+		return false
+	}
+}
+
+// verify that data.AllowAccessFromNS starts with  "at" and is followed by exactly 5 numbers only if it is not empty
+func isValidAllowAccessFromNS(allowAccessFromNS string) bool {
+	if allowAccessFromNS == "" {
+		fmt.Println("AllowAccessFromNS: <empty>")
+		return true
+	}
+	match, _ := regexp.MatchString(`^at[0-9]{5}$`, allowAccessFromNS)
+	if match {
+		fmt.Printf("AllowAccessFromNS: %s\n", allowAccessFromNS)
+		return true
+	} else {
+		fmt.Printf("Invalid AllowAccessFromNS: %s. It should start with 'at' and be followed by exactly 5 numbers.\n", allowAccessFromNS)
+		return false
+	}
+}
+
+// func isValidAllowAccessFromNS(allowAccessFromNS string) bool {
+// 	match, _ := regexp.MatchString(`^at[0-9]{5}$`, allowAccessFromNS)
+// 	if match {
+// 		fmt.Printf("AllowAccessFromNS: %s\n", allowAccessFromNS)
+// 		return true
+// 	} else {
+// 		fmt.Printf("Invalid AllowAccessFromNS: %s. It should start with 'at' and be followed by exactly 5 numbers.\n", allowAccessFromNS)
+// 		return false
+// 	}
+// }
+
+// verify that data.DataClassification is one of the allowed values ("public", "internal", "confidential", or "restricted"
+func isValidDataClassification(dataClassification string) bool {
+	switch dataClassification {
+	case "public", "internal", "confidential", "restricted":
+		fmt.Printf("DataClassification: %s\n", dataClassification)
+		return true
+	default:
+		fmt.Printf("Invalid DataClassification: %s. Allowed values are public, internal, confidential, restricted.\n", dataClassification)
+		return false
+	}
+}
---
+++ namespace-onboarding/2.1.create_files.go
@@ -0,0 +1,318 @@
+package main
+
+// Import necessary packages
+import (
+	"encoding/json" // For JSON processing
+	"fmt"           // For console output
+	"io"
+	"log"
+    "crypto/rand"
+    "encoding/hex"
+	// "log"
+	// "io"
+	"os" // For filesystem operations
+	// "os/exec"
+	"path/filepath" // For file path manipulation
+	"strings"       // For string manipulation
+)
+
+
+// Item represents the structure of the data in the JSON files
+type Item struct {
+	Swci               string `json:"swci"`
+	Suffix             string `json:"suffix"`
+	Region             string `json:"region"`
+	Action             string `json:"action"`
+	AppSubDomain       string `json:"appSubDomain"`
+	SwcID               string `json:"swcID"`
+	RequestedBy        string `json:"requestedBy"`
+	OpEnvironment      string `json:"opEnvironment"`
+	ResourceQuotaCPU 	string `json:"resourceQuotaCPU"`
+	ResourceQuotaMemoryGB      string `json:"resourceQuotaMemoryGB"`
+	ResourceQuotaStorageGB string `json:"resourceQuotaStorageGB"`
+	BillingReference   string `json:"billingReference"`
+	TicketReference    string `json:"ticketReference"`
+	Source             string `json:"source"`
+	AllowAccessFromNS  string `json:"allowAccessFromNS"`
+	DataClassification string `json:"dataClassification"`
+}
+
+func main() {
+	// Specify the directory where the JSON files are located
+	dataDir := "data2"
+
+	// Read all files in the directory
+	files, _ := os.ReadDir(dataDir)
+
+	// Loop over each file
+	for _, f := range files {
+		// Process only JSON files
+		if filepath.Ext(f.Name()) == ".json" {
+			// Construct the full path to the JSON file
+			jsonFile := filepath.Join(dataDir, f.Name())
+
+			// Read the JSON file
+			data, err := os.ReadFile(jsonFile)
+			if err != nil {
+				// If there's an error reading the file, print it and skip to the next file
+				fmt.Println("Error reading file:", err)
+				continue
+			}
+
+			// Unmarshal the JSON data
+			var item Item
+			err = json.Unmarshal(data, &item)
+			if err != nil {
+				fmt.Println("Error unmarshalling JSON:", err)
+				continue
+			}
+
+			// Construct the filename based on the region
+			filename := fmt.Sprintf("region/%s.env", item.Region)
+			// filename := filename1
+			// Read the .env file
+			data, err = os.ReadFile(filename)
+			if err != nil {
+				fmt.Println("Error reading file:", err)
+				return
+			}
+
+			// Split the file contents by line
+			lines := strings.Split(string(data), "\n")
+
+			// Iterate over the lines
+			for _, line := range lines {
+				// Split the line into a key and a value
+				parts := strings.SplitN(line, "=", 2)
+				if len(parts) != 2 {
+					fmt.Println("Invalid line:", line)
+					continue
+				}
+
+				// Set the environment variable
+				key := parts[0]
+				value := parts[1]
+				os.Setenv(key, value)
+			}
+			// create unique idnetifier for each namepace
+			// Create a byte slice with a length of 16
+			b := make([]byte, 16)
+
+			// Generate a random sequence of bytes
+			_, err = rand.Read(b)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			// Encode the byte slice as a hexadecimal string
+			id := hex.EncodeToString(b)
+
+			fmt.Println(id)
+			// if item is uppercase, convert to lowercase
+			item.Action = strings.ToLower(item.Action)
+			item.Swci = strings.ToLower(item.Swci)
+			item.Suffix = strings.ToLower(item.Suffix)
+			item.Region = strings.ToLower(item.Region)
+			item.DataClassification = strings.ToLower(item.DataClassification)
+			item.AppSubDomain = strings.ToLower(item.AppSubDomain)
+			item.AllowAccessFromNS = strings.ToLower(item.AllowAccessFromNS)
+			item.RequestedBy = strings.ToLower(item.RequestedBy)
+			item.OpEnvironment = strings.ToLower(item.OpEnvironment)
+
+			// if item is lowercase, convert to uppercase
+			item.BillingReference = strings.ToUpper(item.BillingReference)
+			item.Source = strings.ToUpper(item.Source)
+			item.SwcID = strings.ToUpper(item.SwcID)
+
+			// if 
+			// Print environment variables
+			clustername := os.Getenv("clustername")
+			domain := os.Getenv("domain")
+			fmt.Println(clustername)
+			fmt.Println(domain)
+
+			// clustername := ""
+			// switch item.Region {
+			// case "westeurope":
+			// 	clustername = "kd469b39473-we01"
+			// case "eastus2":
+			// 	clustername = "kd469b39473-eu01"
+			// case "centralus":
+			// 	clustername = "kd469b39473-cu01"
+			// case "northeurope":
+			// 	clustername = "kd469b39473crd01"
+			// }
+			// domain := "test-akseng-gitops.azpriv-cloud.ubs.net"
+			if item.AppSubDomain == "" {
+				item.AppSubDomain = item.Swci + "-" + item.OpEnvironment + "-" + item.Suffix
+			}
+			if item.Action == "add" || item.Action == "modify" {
+				// Directory to create for the environment
+				dir := fmt.Sprintf("environment/%s/%s/%s-%s", item.Region, clustername, item.Swci, item.Suffix)
+
+				// Create the directory
+				err = os.MkdirAll(dir, os.ModePerm)
+				if err != nil {
+					fmt.Println("Error creating directory:", err)
+					continue
+				}
+				fmt.Println("Creating directory for", dir)
+
+				files, _ := filepath.Glob("kustomize/overlay/*.yaml")
+
+				// Process each YAML file
+				for _, file := range files {
+					// Read the YAML file
+					data, err := os.ReadFile(file)
+					if err != nil {
+						fmt.Println("Error reading file:", err)
+						continue
+					}
+
+					content := string(data)
+					// Replace placeholders in the content
+
+					content = strings.Replace(content, "${swci}", item.Swci, -1)
+					content = strings.Replace(content, "${suffix}", item.Suffix, -1)
+					content = strings.Replace(content, "${region}", item.Region, -1)
+					content = strings.Replace(content, "${clustername}", clustername, -1)
+					content = strings.Replace(content, "${appSubDomain}", item.AppSubDomain, -1)
+					content = strings.Replace(content, "${swcID}", item.SwcID, -1)
+					content = strings.Replace(content, "${requestedBy}", item.RequestedBy, -1)
+					content = strings.Replace(content, "${opEnvironment}", item.OpEnvironment, -1)
+					content = strings.Replace(content, "${resourceQuotaCPU}", item.ResourceQuotaCPU, -1)
+					content = strings.Replace(content, "${resourceQuotaMemoryGB}", item.ResourceQuotaMemoryGB, -1)
+					content = strings.Replace(content, "${resourceQuotaStorageGB}", item.ResourceQuotaStorageGB, -1)
+					content = strings.Replace(content, "${billingReference}", item.BillingReference, -1)
+					content = strings.Replace(content, "${ticketReference}", item.TicketReference, -1)
+					content = strings.Replace(content, "${source}", item.Source, -1)
+					content = strings.Replace(content, "${allowAccessFromNS}", item.AllowAccessFromNS, -1)
+					content = strings.Replace(content, "${dataClassification}", item.DataClassification, -1)
+					content = strings.Replace(content, "${domain}", domain, -1)
+					content = strings.Replace(content, "${action}", item.Action, -1)
+					content = strings.Replace(content, "${id}", id, -1)
+					// Write the modified content to the destination file
+					destFile := filepath.Join(dir, filepath.Base(file))
+					err = os.WriteFile(destFile, []byte(content), os.ModePerm)
+					if err != nil {
+						fmt.Println("Error writing file:", err)
+						continue
+					}
+					os.Remove(filepath.Join(dir, "kustomization-delete.yaml"))
+					// os.Rename(filepath.Join(dir, "kustomization-apptest.yaml"), filepath.Join(dir, "kustomize.yaml"))
+					// os.Rename(filepath.Join(dir, "kustomization-overlay.yaml"), filepath.Join(dir, "kustomization.yaml"))
+					// os.Remove(filepath.Join(dir, "kustomize-overlay"))
+					// os.Remove(filepath.Join(dir, "kustomize-testapp"))
+
+				}
+				if !strings.HasPrefix(item.Suffix, "ob-test") {
+					os.Remove(filepath.Join(dir, "app.yaml"))
+					// os.Rename(filepath.Join(dir, "kustomization-apptest.yaml"), filepath.Join(dir, "kustomize.yaml"))
+					// os.Rename(filepath.Join(dir, "kustomization-overlay.yaml"), filepath.Join(dir, "kustomization.yaml"))
+					os.Remove(filepath.Join(dir, "kustomize-apptest.yaml"))
+				} else {
+					os.Remove(filepath.Join(dir, "kustomize.yaml"))
+					srcFile, err := os.Open(filepath.Join(dir, "kustomize-apptest.yaml"))
+					if err != nil {
+						log.Fatal(err)
+					}
+					defer srcFile.Close()
+
+					dstFile, err := os.Create(filepath.Join(dir, "kustomize.yaml"))
+					if err != nil {
+						log.Fatal(err)
+					}
+					defer dstFile.Close()
+
+					_, err = io.Copy(dstFile, srcFile)
+					if err != nil {
+						log.Fatal(err)
+					}
+					os.Remove(filepath.Join(dir, "kustomization-apptest.yaml"))
+					//neeed to come back and ammend this to allow array of swci at12345, at98765, at55555
+					// If access from NS is allowed and the action is not "Remove"
+					// if item.AllowAccessFromNS != "null" {
+					// // Create a namespace selector
+					// data := []byte(fmt.Sprintf("\n\n- namespaceSelector:\n\n    matchLabels:\n\n      namespace: %s\n\n", item.AllowAccessFromNS))
+					// // Write the namespace selector to a file
+					// err = os.WriteFile(filepath.Join(dir, "network-policy-ns.yaml"), data, os.ModeAppend)
+					// }
+				}
+
+				// cmd := exec.Command("./kustomize/updateAppsKust.sh", "environment/"+item.Region+"/"+clustername, "at")
+				// err := cmd.Run()
+				// if err != nil {
+				// 	log.Fatalf("cmd.Run() failed with %s\n", err)
+				// }
+			} else if item.Action == "remove" {
+				// Directory for the environment to be removed
+				dir := fmt.Sprintf("environment/%s/%s/%s-%s", item.Region, clustername, item.Swci, item.Suffix)
+
+				// Read the delete kustomization file
+				data, _ := os.ReadFile("kustomize/overlay/kustomization-delete.yaml")
+				content := string(data)
+
+				// Replace placeholders in the content
+				content = strings.Replace(content, "${swci}", item.Swci, -1)
+				content = strings.Replace(content, "${suffix}", item.Suffix, -1)
+				content = strings.Replace(content, "${region}", item.Region, -1)
+				content = strings.Replace(content, "${clustername}", clustername, -1)
+				content = strings.Replace(content, "${appSubDomain}", item.AppSubDomain, -1)
+				content = strings.Replace(content, "${swcID}", item.SwcID, -1)
+				content = strings.Replace(content, "${requestedBy}", item.RequestedBy, -1)
+				content = strings.Replace(content, "${opEnvironment}", item.OpEnvironment, -1)
+				content = strings.Replace(content, "${resourceQuotaCPU}", item.ResourceQuotaCPU, -1)
+				content = strings.Replace(content, "${resourceQuotaMemoryGB}", item.ResourceQuotaMemoryGB, -1)
+				content = strings.Replace(content, "${resourceQuotaStorageGB}", item.ResourceQuotaStorageGB, -1)
+				content = strings.Replace(content, "${billingReference}", item.BillingReference, -1)
+				content = strings.Replace(content, "${ticketReference}", item.TicketReference, -1)
+				content = strings.Replace(content, "${source}", item.Source, -1)
+				content = strings.Replace(content, "${allowAccessFromNS}", item.AllowAccessFromNS, -1)
+				content = strings.Replace(content, "${dataClassification}", item.DataClassification, -1)
+				content = strings.Replace(content, "${domain}", domain, -1)
+				content = strings.Replace(content, "${action}", item.Action, -1)
+				content = strings.Replace(content, "${id}", id, -1)
+				// Write the modified content to the kustomization file in the directory
+				os.WriteFile(filepath.Join(dir, "kustomization-delete.yaml"), []byte(content), os.ModePerm)
+
+				// os.Remove(filepath.Join(dir, "kustomize.yaml"))
+				srcFile, err := os.Open(filepath.Join(dir, "kustomization-delete.yaml"))
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer srcFile.Close()
+
+				dstFile, err := os.Create(filepath.Join(dir, "kustomize.yaml"))
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer dstFile.Close()
+
+				_, err = io.Copy(dstFile, srcFile)
+				if err != nil {
+					log.Fatal(err)
+				}
+				// os.Remove(filepath.Join(dir, "kustomization-delete.yaml"))
+				// Find all YAML files in the directory
+				files, _ := filepath.Glob(filepath.Join(dir, "*.yaml"))
+
+				// Delete each file
+				for _, file := range files {
+					// Skip the kustomization.yaml file
+					if filepath.Base(file) != "kustomize.yaml" {
+						os.Remove(file)
+					}
+				}
+				// Log the deletion
+				fmt.Println("Deleting swci and np for", dir)
+				fmt.Println("action == ", item.Action)
+			} else {
+				// If the action is not recognized, log an error and exit
+				fmt.Println("Unknown action operation:", item.Action)
+				os.Exit(1)
+			}
+
+		}
+	}
+}
