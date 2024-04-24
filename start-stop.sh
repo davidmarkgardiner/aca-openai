c9e99956c937dc0fb99290af3cce339a346dbb8a
davidmarkgardiner <davidmarkgardiner@gmail.com>
2024-04-24 14:43:28 +0100

    Update start-stop.sh

--- start-stop.sh
+++ start-stop.sh
@@ -10,6 +10,17 @@ start_clusters() {
         for cluster in $(az aks list -g $rg --query "[].name" -o tsv)
         do
             az aks start -g $rg -n $cluster
+
+            # Delete and recreate Private Endpoints
+            for pe in $(az network private-endpoint list --resource-group $rg --query "[].name" -o tsv)
+            do
+                echo "Deleting Private Endpoint $pe"
+                az network private-endpoint delete --name $pe --resource-group $rg
+
+                echo "Recreating Private Endpoint $pe"
+                # Modify the following command as needed to match your Private Endpoint configuration
+                az network private-endpoint create --name $pe --resource-group $rg --vnet-name YourVnetName --subnet YourSubnetName --private-connection-resource-id $(az aks show --name $cluster --resource-group $rg --query id -o tsv) --group-ids managedClusters
+            done
         done
     done
 }
