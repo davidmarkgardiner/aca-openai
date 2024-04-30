02507131194962727bc9d47acaf63fe0c49ca435
Trigsy1 <davidmarkgardiner@gmail.com>
2024-04-30 16:26:50 +0100

    latest working code

---
+++ namespace-onboarding/webhook/updateKust/2.2.updateKust.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+    // "encoding/json"
+    "fmt"
+    "log"
+    "os"
+    "strings"
+    // "path/filepath"
+
+    "gopkg.in/yaml.v2"
+)
+
+// Data struct represents the structure of the JSON data
+type Item struct {
+    Region        string 
+    OpEnvironment string 
+    Swci          string 
+    Suffix        string 
+}
+
+func main() {
+            vars := []string{
+                "ACTION",
+                "SWCI",
+                "SUFFIX",
+                "REGION",
+                "OP_ENVIRONMENT",
+                "RESOURCE_QUOTA_CPU",
+                "RESOURCE_QUOTA_MEMORY_GB",
+                "RESOURCE_QUOTA_STORAGE_GB",
+                "BILLING_REFERENCE",
+                "SOURCE",
+                "SWC_ID",
+                "DATA_CLASSIFICATION",
+                "APP_SUBDOMAIN",
+                "ALLOW_ACCESS_FROM_NS",
+                "REQUESTED_BY",
+            }
+
+            for _, v := range vars {
+                fmt.Printf("%s: %s\n", v, os.Getenv(v))
+            }
+
+            item := Item{
+                Swci:                   os.Getenv("SWCI"),
+                Suffix:                 os.Getenv("SUFFIX"),
+                Region:                 os.Getenv("REGION"),
+                OpEnvironment:          os.Getenv("OP_ENVIRONMENT"),
+            }
+
+            var data []byte
+            var err error
+            // Construct the filename based on the region
+            filename := fmt.Sprintf("../region/%s.env", item.Region)
+            // filename := filename1
+            // Read the .env file
+            data, err = os.ReadFile(filename)
+            if err != nil {
+                fmt.Println("Error reading file:", err)
+                return
+            }
+
+            // Split the file contents by line
+            lines := strings.Split(string(data), "\n")
+
+            // Iterate over the lines
+            for _, line := range lines {
+                // Split the line into a key and a value
+                parts := strings.SplitN(line, "=", 2)
+                if len(parts) != 2 {
+                    fmt.Println("Invalid line:", line)
+                    continue
+                }
+
+                // Set the environment variable
+                key := parts[0]
+                value := parts[1]
+                os.Setenv(key, value)
+            }
+            clustername := os.Getenv("clustername")
+            // Read the kustomization.yaml file
+            data, err = os.ReadFile("../environment/" + item.Region + "/" + clustername + "/kustomization.yaml")
+            if err != nil {
+                log.Fatalf("Failed to read kustomization.yaml: %v", err)
+            }
+
+            // Unmarshal the YAML data into a map
+            kustomization := make(map[string]interface{})
+            err = yaml.Unmarshal(data, &kustomization)
+            if err != nil {
+                log.Fatalf("Failed to unmarshal YAML data: %v", err)
+            }
+
+            // Construct the new resource name
+            resource := []string{item.Swci + "-" + item.OpEnvironment + "-" + item.Suffix}
+
+            // Get the existing resources from the kustomization map
+            existingResources, ok := kustomization["resources"].([]interface{})
+            if !ok {
+                log.Fatalf("Failed to convert resources to slice")
+            }
+            
+            // Check if the new resource already exists in the resources
+            resourceExists := false
+            for _, r := range existingResources {
+                if r == resource[0] {
+                    resourceExists = true
+                    break
+                }
+            }
+            
+            // If the resource doesn't exist, append it to the resources
+            if !resourceExists {
+                existingResources = append(existingResources, resource[0])
+            }
+            
+            // Update the resources in the kustomization map
+            kustomization["resources"] = existingResources
+
+            // Marshal the kustomization map back into YAML
+            data, err = yaml.Marshal(&kustomization)
+            if err != nil {
+                log.Fatalf("Failed to marshal YAML data: %v", err)
+            }
+
+            // Write the updated YAML back to the kustomization.yaml file
+            err = os.WriteFile("../environment/"+item.Region+"/"+clustername+"/kustomization.yaml", data, 0644)
+            if err != nil {
+                log.Fatalf("Failed to write to kustomization.yaml: %v", err)
+            }
+        }
---
+++ namespace-onboarding/webhook/validation/validation_test.go
@@ -0,0 +1,296 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"testing"
+)
+
+type Data struct {
+	Action                 string
+	Swci                   string
+	Suffix                 string
+	Region                 string
+	OpEnvironment          string
+	ResourceQuotaCPU       string
+	ResourceQuotaMemoryGB  string
+	ResourceQuotaStorageGB string
+	BillingReference       string
+	Source                 string
+	SwcID                  string
+	DataClassification     string
+	AppSubdomain           string
+	AllowAccessFromNS      string
+	RequestedBy            string
+}
+
+func TestJsonValidationAllFiles(t *testing.T) {
+
+	vars := []string{
+		"ACTION",
+		"SWCI",
+		"SUFFIX",
+		"REGION",
+		"OP_ENVIRONMENT",
+		"RESOURCE_QUOTA_CPU",
+		"RESOURCE_QUOTA_MEMORY_GB",
+		"RESOURCE_QUOTA_STORAGE_GB",
+		"BILLING_REFERENCE",
+		"SOURCE",
+		"SWC_ID",
+		"DATA_CLASSIFICATION",
+		"APP_SUBDOMAIN",
+		"ALLOW_ACCESS_FROM_NS",
+		"REQUESTED_BY",
+	}
+
+	for _, v := range vars {
+		fmt.Printf("%s: %s\n", v, os.Getenv(v))
+	}
+
+	data := Data{
+		Action:                 os.Getenv("ACTION"),
+		Swci:                   os.Getenv("SWCI"),
+		Suffix:                 os.Getenv("SUFFIX"),
+		Region:                 os.Getenv("REGION"),
+		OpEnvironment:          os.Getenv("OP_ENVIRONMENT"),
+		ResourceQuotaCPU:       os.Getenv("RESOURCE_QUOTA_CPU"),
+		ResourceQuotaMemoryGB:  os.Getenv("RESOURCE_QUOTA_MEMORY_GB"),
+		ResourceQuotaStorageGB: os.Getenv("RESOURCE_QUOTA_STORAGE_GB"),
+		BillingReference:       os.Getenv("BILLING_REFERENCE"),
+		Source:                 os.Getenv("SOURCE"),
+		SwcID:                  os.Getenv("SWC_ID"),
+		DataClassification:     os.Getenv("DATA_CLASSIFICATION"),
+		AppSubdomain:           os.Getenv("APP_SUBDOMAIN"),
+		AllowAccessFromNS:      os.Getenv("ALLOW_ACCESS_FROM_NS"),
+		RequestedBy:            os.Getenv("REQUESTED_BY"),
+	}
+
+
+	t.Logf("Action: %s\n", data.Action)
+	t.Logf("Swci: %s\n", data.Swci)
+	t.Logf("Suffix: %s\n", data.Suffix)
+	t.Logf("Region: %s\n", data.Region)
+	t.Logf("OpEnvironment: %s\n", data.OpEnvironment)
+	t.Logf("ResourceQuotaCPU: %s\n", data.ResourceQuotaCPU)
+	t.Logf("ResourceQuotaMemoryGB: %s\n", data.ResourceQuotaMemoryGB)
+	t.Logf("ResourceQuotaStorageGB: %s\n", data.ResourceQuotaStorageGB)
+	t.Logf("BillingReference: %s\n", data.BillingReference)
+	t.Logf("Source: %s\n", data.Source)
+	t.Logf("Swc: %s\n", data.SwcID)
+	t.Logf("DataClassification: %s\n", data.DataClassification)
+	t.Logf("AppSubdomain: %s\n", data.AppSubdomain)
+	t.Logf("AllowAccessFromNS: %s\n", data.AllowAccessFromNS)
+	t.Logf("RequestedBy: %s\n", data.RequestedBy)
+
+	if !isValidAction(data.Action) || !isValidSwci(data.Swci) || !isValidSuffix(data.Suffix) ||
+	!isValidRegion(data.Region) || !isValidOpEnvironment(data.OpEnvironment) ||
+	!isValidResourceQuotaCPU(data.ResourceQuotaCPU) ||!isValidResourceQuotaMemoryGB(data.ResourceQuotaMemoryGB) ||!isValidResourceQuotaStorageGB(data.ResourceQuotaStorageGB) ||
+	!isValidBillingReference(data.BillingReference) ||
+	!isValidRequestedBy(data.RequestedBy) || !isValidAllowAccessFromNS(data.AllowAccessFromNS) ||
+	!isValidDataClassification(data.DataClassification) {
+	t.Fail()
+	}
+}
+
+
+// verify that data.Action is one of the allowed values ("Add", "Remove", or "Update")
+func isValidAction(action string) bool {
+switch action {
+case "add", "remove", "update":
+	return true
+default:
+	fmt.Printf("Invalid action: %s. Allowed values are Add, Remove, Update.\n", action)
+	return false
+}
+}
+
+// verify that data.Swci starts with "at" and is followed by exactly 5 numbers,
+func isValidSwci(swci string) bool {
+match, err := regexp.MatchString(`^at\d{5}$`, swci)
+if err != nil {
+	fmt.Println("Error while matching regex:", err)
+	return false
+}
+if match {
+	return true
+} else {
+	fmt.Printf("Invalid Swci: %s. It should start with 'at' followed by exactly 5 numbers.\n", swci)
+	return false
+}
+}
+
+// verify that data.Suffix is a maximum of 8 characters and contains only numbers, letters, and hyphens
+func isValidSuffix(suffix string) bool {
+match, _ := regexp.MatchString(`^[a-zA-Z0-9-]{1,8}$`, suffix)
+if match {
+	fmt.Printf("Suffix: %s\n", suffix)
+	return true
+} else {
+	fmt.Printf("Invalid Suffix: %s. It should be a maximum of 8 characters and contain only numbers, letters, and hyphens.\n", suffix)
+	return false
+}
+}
+
+// verify that data.Region is one of the allowed values ("westeurope", "eastus2", or "centralus")
+func isValidRegion(region string) bool {
+switch region {
+case "westeurope", "eastus2", "centralus":
+	fmt.Printf("Region: %s\n", region)
+	return true
+default:
+	fmt.Printf("Invalid region: %s. Allowed values are westeurope, eastus2, centralus.\n", region)
+	return false
+}
+}
+func isValidOpEnvironment(opEnvironment string) bool {
+// verify that data.OPEnvironment is one of the allowed values ("dev", "prod", "te1", or "te2"),
+switch opEnvironment {
+case "dev", "prod", "te1", "te2":
+	fmt.Printf("OPEnvironment: %s\n", opEnvironment)
+	return true
+default:
+	fmt.Printf("Invalid OPEnvironment: %s. Allowed values are dev, prod, te1, te2.\n", opEnvironment)
+	return false
+}
+}
+
+// // data.ResourceQuota is one of the allowed values ("small", "medium", or "large")
+// func isValidResourceQuota(resourceQuota string) bool {
+// 	switch resourceQuota {
+// 	case "small", "medium", "large":
+// 		fmt.Printf("ResourceQuota: %s\n", resourceQuota)
+// 		return true
+// 	default:
+// 		fmt.Printf("Invalid ResourceQuota: %s. Allowed values are small, medium, large.\n", resourceQuota)
+// 		return false
+// 	}
+// }
+
+// verify that data.BillingReference is in the format "AB-BC-ABCDE-ABCDE" and contains only letters,
+func isValidBillingReference(billingReference string) bool {
+match, _ := regexp.MatchString(`^[A-Za-z]{2}-[A-Za-z]{2}-[A-Za-z]{5}-[A-Za-z]{5}$`, billingReference)
+if match {
+	fmt.Printf("BillingReference: %s\n", billingReference)
+	return true
+} else {
+	fmt.Printf("Invalid BillingReference: %s. It should be in the format AB-BC-DEPT-ABCDE and contain only letters.\n", billingReference)
+	return false
+}
+}
+
+// verify that data.RequestedBy is an email like "david.gardiner@ubs.com", a GPN like "43709651", or a T number like "t69332"
+func isValidRequestedBy(requestedBy string) bool {
+emailMatch, _ := regexp.MatchString(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`, requestedBy)
+gpnMatch, _ := regexp.MatchString(`^[0-9]{8}$`, requestedBy)
+tNumberMatch, _ := regexp.MatchString(`^t[0-9]{5}$`, requestedBy)
+
+if emailMatch || gpnMatch || tNumberMatch {
+	fmt.Printf("RequestedBy: %s\n", requestedBy)
+	return true
+} else {
+	fmt.Printf("Invalid RequestedBy: %s. It should be an email like david.gardiner@ubs.com, a GPN like 43709651, or a T number like t69332.\n", requestedBy)
+	return false
+}
+}
+
+// verify that data.AllowAccessFromNS starts with  "at" and is followed by exactly 5 numbers only if it is not empty
+func isValidAllowAccessFromNS(allowAccessFromNS string) bool {
+if allowAccessFromNS == "" {
+	fmt.Println("AllowAccessFromNS: <empty>")
+	return true
+}
+match, _ := regexp.MatchString(`^at[0-9]{5}$`, allowAccessFromNS)
+if match {
+	fmt.Printf("AllowAccessFromNS: %s\n", allowAccessFromNS)
+	return true
+} else {
+	fmt.Printf("Invalid AllowAccessFromNS: %s. It should start with 'at' and be followed by exactly 5 numbers.\n", allowAccessFromNS)
+	return false
+}
+}
+
+// func isValidAllowAccessFromNS(allowAccessFromNS string) bool {
+// 	match, _ := regexp.MatchString(`^at[0-9]{5}$`, allowAccessFromNS)
+// 	if match {
+// 		fmt.Printf("AllowAccessFromNS: %s\n", allowAccessFromNS)
+// 		return true
+// 	} else {
+// 		fmt.Printf("Invalid AllowAccessFromNS: %s. It should start with 'at' and be followed by exactly 5 numbers.\n", allowAccessFromNS)
+// 		return false
+// 	}
+// }
+
+// verify that data.DataClassification is one of the allowed values ("public", "internal", "confidential", or "restricted"
+func isValidDataClassification(dataClassification string) bool {
+switch dataClassification {
+case "public", "internal", "confidential", "restricted":
+	fmt.Printf("DataClassification: %s\n", dataClassification)
+	return true
+default:
+	fmt.Printf("Invalid DataClassification: %s. Allowed values are public, internal, confidential, restricted.\n", dataClassification)
+	return false
+}
+}
+// verify that data.ResourceQuotaStorageGB is between 0-100 GB
+func isValidResourceQuotaStorageGB(resourceQuotaStorageGB string) bool {
+match, _ := regexp.MatchString(`^(0|[1-9][0-9]?|100)$`, resourceQuotaStorageGB)
+if match {
+	fmt.Printf("ResourceQuotaStorageGB: %s\n", resourceQuotaStorageGB)
+	return true
+} else {
+	fmt.Printf("Invalid ResourceQuotaStorageGB: %s. It should be between 0-100 GB.\n", resourceQuotaStorageGB)
+	return false
+}
+}
+// verify that resourceQuotaCPU is between 0.1 and 8
+func isValidResourceQuotaCPU(resourceQuotaCPU string) bool {
+match, _ := regexp.MatchString(`^(0\.[1-9]|1(\.0)?|[2-8])$`, resourceQuotaCPU)
+if match {
+	fmt.Printf("ResourceQuotaCPU: %s\n", resourceQuotaCPU)
+	return true
+} else {
+	fmt.Printf("Invalid ResourceQuotaCPU: %s. It should be between 0.1 and 8.\n", resourceQuotaCPU)
+	return false
+}
+}
+
+
+// func isValidResourceQuotaCPU(resourceQuotaCPU string) bool {
+// 	// match, _ := regexp.MatchString(`^(100m|[1-9][0-9]?[0-9]?m|1Gi|2Gi|3Gi|4Gi|5Gi|6Gi|7Gi|8Gi)$`, resourceQuotaCPU)
+// 	match, _ := regexp.MatchString(`^(100m|[1-9][0-9]?[0-9]?m|1Gi|2Gi|3Gi|4Gi|5Gi|6Gi|7Gi|8Gi)$`, resourceQuotaCPU)
+// 	if match {
+// 		fmt.Printf("ResourceQuotaCPU: %s\n", resourceQuotaCPU)
+// 		return true
+// 	} else {
+// 		fmt.Printf("Invalid ResourceQuotaCPU: %s. It should be between 100m-1000m and 1Gi-8Gi.\n", resourceQuotaCPU)
+// 		return false
+// 	}
+// }
+
+
+// func isValidResourceQuotaCPU(resourceQuotaCPU string) bool {
+// 	match, _ := regexp.MatchString(`^(0|[1-8])$`, resourceQuotaCPU)
+// 	if match {
+// 		fmt.Printf("ResourceQuotaCPU: %s\n", resourceQuotaCPU)
+// 		return true
+// 	} else {
+// 		fmt.Printf("Invalid ResourceQuotaCPU: %s. It should be between 0-8.\n", resourceQuotaCPU)
+// 		return false
+// 	}
+// }
+
+
+// verify that resourceQuotaMemoryGB is between 0-64 GB
+func isValidResourceQuotaMemoryGB(resourceQuotaMemoryGB string) bool {
+// match, _ := regexp.MatchString(`^(100m|[1-9][0-9]?[0-9]?m|[1-9]Gi|[1-5][0-9]Gi|6[0-4]Gi)$`, resourceQuotaMemoryGB)
+match, _ := regexp.MatchString(`^([0-9]|[1-5][0-9]|6[0-4])$`, resourceQuotaMemoryGB)
+if match {
+	fmt.Printf("ResourceQuotaMemoryGB: %s\n", resourceQuotaMemoryGB)
+	return true
+} else {
+	fmt.Printf("Invalid ResourceQuotaMemoryGB: %s. It should be between 0-64 GB.\n", resourceQuotaMemoryGB)
+	return false
+}
+}
