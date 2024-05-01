7fb364e7139c8edb4b2c2205aa94c04cdd038dbe
Trigsy1 <davidmarkgardiner@gmail.com>
2024-05-01 17:33:13 +0100

    Refactor code to improve performance and readability

---
+++ namespace-onboarding/webhook/apptest/4.test_app.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+    "strings"
+    "time"
+)
+
+type Item struct {
+    Action        string
+    Swci          string
+    Suffix        string
+    Region        string
+    OpEnvironment string
+}
+
+func checkHttpResponse(url string, item Item) error {
+    resp, err := http.Get(url)
+    if err != nil {
+        return fmt.Errorf("Failed to send request: %v", err)
+    }
+    defer resp.Body.Close()
+
+    if resp.StatusCode != http.StatusOK {
+        return fmt.Errorf("URL %s did not return status code 200, got: %d", url, resp.StatusCode)
+    }
+
+    return nil
+}
+
+func main() {
+    vars := []string{
+        "ACTION",
+        "SWCI",
+        "SUFFIX",
+        "REGION",
+        "OP_ENVIRONMENT",
+    }
+
+    for _, v := range vars {
+        fmt.Printf("%s: %s\n", v, os.Getenv(v))
+    }
+
+    item := Item{
+        Action:        os.Getenv("ACTION"),
+        Swci:          os.Getenv("SWCI"),
+        Suffix:        os.Getenv("SUFFIX"),
+        Region:        os.Getenv("REGION"),
+        OpEnvironment: os.Getenv("OP_ENVIRONMENT"),
+    }
+
+    filename := fmt.Sprintf("region/%s.env", item.Region)
+    fileContent, err := os.ReadFile(filename)
+    if err != nil {
+        log.Fatalf("Error reading file: %v", err)
+    }
+
+    lines := strings.Split(string(fileContent), "\n")
+    for _, line := range lines {
+        parts := strings.SplitN(line, "=", 2)
+        if len(parts) != 2 {
+            fmt.Println("Invalid line:", line)
+            continue
+        }
+
+        key := parts[0]
+        value := parts[1]
+        os.Setenv(key, value)
+    }
+
+    domain := os.Getenv("domain")
+
+    if strings.HasPrefix(item.Suffix, "ob-test") && (item.Action == "Add") {
+        for i := 0; i < 10; i++ {
+            url := fmt.Sprintf("https://%s.%s.%s.%s", item.Swci, item.OpEnvironment, item.Suffix, domain)
+            log.Printf("checking https Response from URL: %s", url)
+            err = checkHttpResponse(url, item)
+            if err == nil {
+                break
+            }
+
+            time.Sleep(30 * time.Second)
+        }
+
+        if err != nil {
+            log.Fatal(err)
+        } else {
+            log.Printf("App on %s, url up and running", url)
+        }
+    }
+}
---
+++ namespace-onboarding/webhook/rbac/3.2.rbac.rolebinding.ps1
@@ -0,0 +1,92 @@
+function Get-AccessTokenForFunction {
+    param(
+        $clientid,
+        $scope,
+        $tenantID,
+        $secret,
+        $requestAccessTokenUri
+    )
+
+    $body = "grant_type=client_credentials&client_id=$clientid&client_secret=$secret&scope=$scope"
+    $accessTokenData = irm -method Post -uri $requestAccessTokenUri -Body $body -ContentType 'application/x-www-form-urlencoded'
+    if ($accessTokenData.access_token) {
+        return $accessTokenData.access_token
+    } else {
+        throw "Failed to get access token"
+    }
+}
+
+function MakeApiCall {
+    param(
+        $apiEndpoint,
+        $accessToken,
+        $functionBody
+    )
+
+    $response = Invoke-RestMethod -Uri $apiEndpoint -Headers @{"authorization" = "Bearer $($accessToken)"} -Body $functionBody -ContentType "application/json" -Method POST -Verbose
+    if ($null -eq $response) {
+        throw "API call failed"
+    }
+    return $response
+}
+
+function ProcessEnvVars {
+    param(
+        $regionData,
+        $accessToken,
+        $apiEndpoint
+    )
+
+    $action = $env:ACTION
+    $namespace = $env:NAMESPACE
+    $suffix = $env:SUFFIX
+    $region = $env:REGION
+
+    # Convert the first letter to uppercase and the rest to lowercase
+    $action = $action.Substring(0,1).ToUpper() + $action.Substring(1).ToLower()
+    # Convert $namespace $suffix $region to lowercase
+    $namespace = $namespace.ToLower()
+    $suffix = $suffix.ToLower()
+    $region = $region.ToLower()
+
+    if ($regionData.ContainsKey($region)) {
+        $clusterid = $regionData[$region]["clusterid"]
+        $sub = $regionData[$region]["sub"]
+        $rg = $regionData[$region]["rg"]
+    } else {
+        Write-Host "Invalid Cluster Name"
+        return
+    }
+
+    Write-Host "region: $region"
+    
+    $functionBody = @{
+        subscriptionID     = "$sub"
+        resourceGroupName  = "$rg"
+        action             = "$action"
+        aksname            = "$clusterid"
+        namespacename      = "$namespace"
+        suffix             = "$suffix"
+    } | convertto-json
+
+    $namespaces_file = "environment/${region}/${clusterid}/kustomization.yaml"
+
+    if ($action -eq "add") {
+        if (!(Select-String -Path $namespaces_file -Pattern $namespace-$suffix -Quiet)) {
+            $response = MakeApiCall -apiEndpoint $apiEndpoint -accessToken $accessToken -functionBody $functionBody
+            Write-Host "$namespace-$env rbac role assignment added"
+        }
+    }
+    if ($action -eq "remove") {
+        if (Select-String -Path $namespaces_file -Pattern $namespace-$suffix -Quiet) {
+            $response = MakeApiCall -apiEndpoint $apiEndpoint -accessToken $accessToken -functionBody $functionBody
+            Write-Host "$namespace-$env rbac role assignment removed"
+        }
+    }
+}
+
+# Main script
+# $accessToken = Get-AccessTokenForFunction -scope 
+# $apiEndpoint = https://azurewebsites.net/api/aksnamespaceroleassign
+
+# ProcessEnvVars -regionData $regionData -accessToken $accessToken -apiEndpoint $apiEndpoint
