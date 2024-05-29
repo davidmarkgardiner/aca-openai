aa5d5801ad5536213347f3a9db3582bf94a8cd11
Trigsy1 <davidmarkgardiner@gmail.com>
2024-05-29 18:02:19 +0100

    chore: Update test_script.ps1 to handle error from server in Azure DevOps API request

--- namespace-onboarding/webhook/invokeRestMethod/test_script.ps1
+++ namespace-onboarding/webhook/invokeRestMethod/test_script.ps1
@@ -2,3 +2,5 @@
 
 
 Invoke-RestMethod -Uri $uri -Method Post -Body $json -ContentType "application/json" -Headers $authHeader
+
+Error from server (InternalError): error when creating ".": Internal error occurred: failed calling webhook "default.v1api20220301.serverfarms.web.azure.com": failed to call webhook: the server could not find the requested resource
