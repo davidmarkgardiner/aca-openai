d634a67e61fd054946224cecf534a6bb13897930
Trigsy1 <davidmarkgardiner@gmail.com>
2024-06-03 16:02:39 +0100

    chore: Update .gitignore to include payload_variables.sh

---
+++ namespace-onboarding/webhook/invokeRestMethod/payload_variables.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+
+#!/bin/bash
+
+variables=$(cat <<EOF
+{
+    "ACTION": "Add",
+    "SWCI": "at65999",
+    "SUFFIX": "poc1",
+    "REGION": "westeurope",
+    "OPENV": "dev",
+    "CPU": "500m",
+    "MEM": "3",
+    "STORAGE": "128",
+    "BILLINGREFERENCE": "AB-BC-ABCDE-ABCDE",
+    "SOURCE": "GSNOW",
+    "SWCID": "AA98765",
+    "CLASS": "public",
+    "SUBDOMAIN": "",
+    "ALLOWACCESSFROMNS": "at98765",
+    "REQUESTEDBY": ""
+}
+EOF
+)
+
+body=$(cat <<EOF
+{
+  "definition": {"id": 2},
+  "sourceBranch": "refs/heads/master",
+  "parameters": $variables
+}
+EOF
+)
+
+echo $body
+
+# Define the payload
+# payload=$(jq -n \
+#   --arg id "2" \
+#   # --arg sourceBranch "refs/heads/dev" \
+#   --arg sourceBranch "refs/heads/master" \
+#   --argjson parameters "$(echo $parameters)" \
+#   '{
+#     "definition": {"id": $id},
+#     "sourceBranch": $sourceBranch,
+#     "parameters": $parameters
+#   }')
+# # Define the payload
+# payload=$(jq -n \
+#   --arg id "2" \
+#   --arg sourceBranch "refs/heads/dev" \
+#   --argjson parameters '{
+#     "action": "Add",
+#     "swci": "at65999",
+#     "suffix": "poc1",
+#     "region": "westeurope",
+#     "opEnvironment": "dev",
+#     "resourceQuotaCPU": "500m",
+#     "resourceQuotaMemoryGB": "3",
+#     "resourceQuotaStorageGB": "128",
+#     "billingReference": "AB-BC-ABCDE-ABCDE",
+#     "source": "GSNOW",
+#     "swcID": "AA98765",
+#     "dataClassification": "public",
+#     "appSubdomain": "",
+#     "allowAccessFromNS": "at98765",
+#     "requestedBy": "david.gardiner@ubs.com"
+#   }' \
+#   '{
+#     "definition": {"id": $id},
+#     "sourceBranch": $sourceBranch,
+#     "parameters": $parameters
+#   }')
+
+#   # --arg sourceBranch "refs/heads/dev" \
+# # Define the payload
+# payload=$(jq -n \
+#   --arg id "2" \
+#   --arg sourceBranch "master" \
+#   --arg action "Add" \
+#   --arg swci "at65999" \
+#   --arg suffix "poc1" \
+#   --arg region "westeurope" \
+#   --arg opEnvironment "dev" \
+#   --arg resourceQuotaCPU "500m" \
+#   --arg resourceQuotaMemoryGB "3" \
+#   --arg resourceQuotaStorageGB "128" \
+#   --arg billingReference "AB-BC-ABCDE-ABCDE" \
+#   --arg source "GSNOW" \
+#   --arg swcID "AA98765" \
+#   --arg dataClassification "public" \
+#   --arg appSubdomain "" \
+#   --arg allowAccessFromNS "at98765" \
+#   --arg requestedBy "david.gardiner@ubs.com" \
+#   '{
+#     "definition": {"id": $id},
+#     "sourceBranch": $sourceBranch,
+#     "action": $action,
+#     "swci": $swci,
+#     "suffix": $suffix,
+#     "region": $region,
+#     "opEnvironment": $opEnvironment,
+#     "resourceQuotaCPU": $resourceQuotaCPU,
+#     "resourceQuotaMemoryGB": $resourceQuotaMemoryGB,
+#     "resourceQuotaStorageGB": $resourceQuotaStorageGB,
+#     "billingReference": $billingReference,
+#     "source": $source,
+#     "swcID": $swcID,
+#     "dataClassification": $dataClassification,
+#     "appSubdomain": $appSubdomain,
+#     "allowAccessFromNS": $allowAccessFromNS,
+#     "requestedBy": $requestedBy
+#   }')
+
+# write ouput of payload
+echo "$payload"
+
+# Define the URI for the Azure DevOps API
+uri="https://dev.azure.com/home-k8s/Homelab/_apis/build/builds?api-version=6.0"
+
+# Get the Azure access token
+token=$(az account get-access-token --resource "https://management.azure.com/" | jq -r '.accessToken')
+
+# Define the authorization header for the API request
+authHeader="Authorization: Bearer $token"
+
+# Make the REST call to Azure DevOps API
+# response=$(curl -s -w "%{http_code}" -X POST -d "$payload" -H "Content-Type: application/json" -H "$authHeader" "$uri")
+# response=$(curl -L -s -w "%{http_code}" -X POST -d "$payload" -H "Content-Type: application/json" -H "$authHeader" "$uri")
+response=$(curl -L -s -w "%{http_code}" -X POST -d "$payload" -H "Content-Type: application/json" -H "Content-Length: ${#payload}" -H "$authHeader" "$uri")
+# Extract the status code from the response
+statusCode=$(echo "$response" | tail -n1)
+
+# Print the status code
+echo "$statusCode"
+
+# If the status code is 200, print a success message and the response
+if [ "$statusCode" -eq 200 ]; then
+    echo "API call successful"
+    echo "$response"
+fi
